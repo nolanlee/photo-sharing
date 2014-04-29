f7c10b70241073ae687c8a219e805360023e48d4
Schiffer.Li <schiffer.li@sap.com>
2014-04-29 16:43:42 +0800

    Revert "init project"
    This reverts commit bf9c257e250e0d91ed64d312e3363c3ef5a924f0.

--- app/models/admin.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
-
-var adminSchema = new Schema({
-  username: String,
-  password: String
-});
-
-mongoose.model('Admin', adminSchema);
